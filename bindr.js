d17a6cb52071a00926a878459e177d6325b7064d
Gian Acuna <gian@gianacuna.com>
2014-08-21 03:43:57 +0800

    Added parsing of form
    Form will be parsed and each element will be converted to bindr object

--- bindr.js
+++ bindr.js
@@ -10,6 +10,10 @@ Bindr.Bindr =  function (elemID){
 		return handlr(this.elem);
 	};
 	this.set = function(val){
+		var handlr = Bindr.Set(this.elem.type);
+		return handlr(this.elem, val);
+	};
+	this.clear = function(){
 
 	}
 };
@@ -21,7 +25,7 @@ Bindr.Get = function(elemName){
 		},
 		'textarea': function(elem){
 			return elem.value;
-		}
+		},
 		'checkbox': function(elem){
 			return elem.checked;
 		},
@@ -45,8 +49,8 @@ Bindr.Set = function(elemName){
 			elem.checked = val;
 		},
 		'radio': function(elem, val){
-			var radios = document.querySelectorAll('input[name = "' + elem.name + '"]:checked').value;
-			for(var i = 0; i <= radios.length; i++)
+			var radios = document.querySelectorAll('input[name = "' + elem.name + '"]');
+			for(var i = 0; i <= radios.length - 1; i++)
 			{
 				if (radios[i].value == val){
 					radios[i].checked = true;
@@ -54,4 +58,21 @@ Bindr.Set = function(elemName){
 			}
 		}
 	};
+
+	return this.handles[elemName];
+}
+
+Bindr.FormBndr = function(formID){
+	this.elements = {};
+	this.initialize = function(){
+		var formElements = Array.prototype.slice.call(document.getElementById(formID).elements);
+		var that = this;
+		formElements.forEach(function(x){
+			var propkey = (x.type == 'radio' && x.name) || x.id;
+			that.formElements[propkey] = new Bindr.Bindr((x.type == 'radio' && x.id) || propkey);
+		});
+		console.log(that.elements);
+	};
+
+	this.initialize();
 }
